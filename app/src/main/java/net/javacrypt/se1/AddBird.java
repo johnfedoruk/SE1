468d8790d97cf15f9104c138bb1f99721b13c508
johnfedoruk <johnny@johnfedoruk.ca>
2016-02-04 12:17:27 -0600

    Added intents for: adding birds searching birds

--- app/src/main/java/net/javacrypt/se1/AddBird.java
+++ app/src/main/java/net/javacrypt/se1/AddBird.java
@@ -1,26 +1,23 @@
 package net.javacrypt.se1;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.EditText;
 
 
-public class MainActivity extends ActionBarActivity {
+public class AddBird extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_add_bird);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_add_bird, menu);
         return true;
     }
 
@@ -37,23 +34,5 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * Allows the user to add a bird
-     * @param view
-     */
-    public void openAddBird(View view) {
-        Intent intent = new Intent(this,AddBird.class);
-        startActivity(intent);
-    }
-
-    /**
-     * Allows the user to add a bird
-     * @param view
-     */
-    public void openSearchBird(View view) {
-        Intent intent = new Intent(this,SearchBird.class);
-        startActivity(intent);
     }
 }
