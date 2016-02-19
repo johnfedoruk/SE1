10b55e0ab8ab7d5ecfdd44c10bbf5f398bee67d3
johnfedoruk <johnny@johnfedoruk.ca>
2016-02-19 02:49:09 -0600

    Search Experiment GUI added.

---
+++ app/src/main/java/src/businessLogicLayer/SearchExperiment.java
@@ -0,0 +1,48 @@
+package src.businessLogicLayer;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import net.javacrypt.se1.R;
+
+
+public class SearchExperiment extends ActionBarActivity {
+    public final static String EXTRA_MESSAGE = "net.javacrypt.se1.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_experiment);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_search_experiment, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Allows the user to open an experiment
+     * @param view
+     */
+    public void openViewExperiment(View view) {
+        Intent intent = new Intent(this,ViewExperiment.class);
+        String experimentId = "Experiment 1";
+        intent.putExtra(EXTRA_MESSAGE,experimentId);
+        startActivity(intent);
+    }
+}
