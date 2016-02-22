afbac0b43e9ec252591aa92930c2e22b04a17907
Ratstache <landonbutterworth@gmail.com>
2016-02-22 03:01:31 -0600

    completes search bird and search experiment implementation in database layer

--- app/src/main/java/databaseLayer/DatabaseManager.java
+++ app/src/main/java/databaseLayer/DatabaseManager.java
@@ -18,11 +18,11 @@ public class DatabaseManager {
         experimentList = new ArrayList<Experiment>();
         cal = Calendar.getInstance();
 
-        birdList.add(new Bird("0001", "bird1","Experiment #1",getCalendar(2016,2,22),getCalendar(2016,2,22),"Female"));
-        birdList.add(new Bird("0002", "bird2","Experiment #2",getCalendar(2016,2,23),getCalendar(2016,2,24),"Male"));
-        birdList.add(new Bird("0003", "bird3","Experiment #3",getCalendar(2016,2,24),getCalendar(2016,2,25),"Female"));
-        experimentList.add(new Experiment("Dying Bird","Psychological","Group 1",getCalendar(2016,2,22),getCalendar(2016,2,22),"John,Gimli","blahblah"));
-        experimentList.add(new Experiment("Living Bird","Suicidal","Group 2",getCalendar(2016,2,22),getCalendar(2016,2,22),"James,Angelo","blahblah"));
+        birdList.add(new Bird("0001", "bird1", "Experiment #1", getCalendar(2016,2,22), getCalendar(2016,2,22), "Female"));
+        birdList.add(new Bird("0002", "bird2", "Experiment #2", getCalendar(2016,2,23), getCalendar(2016,2,24), "Male"));
+        birdList.add(new Bird("0003", "bird3", "Experiment #3", getCalendar(2016,2,24), getCalendar(2016,2,25), "Female"));
+        experimentList.add(new Experiment("Dying Bird", "Psychological","Group 1", getCalendar(2016,2,22), getCalendar(2016,2,22), "John,Gimli", "blahblah"));
+        experimentList.add(new Experiment("Living Bird", "Suicidal","Group 2", getCalendar(2016,2,22), getCalendar(2016,2,22), "James,Angelo", "blahblah"));
     }
 
     public void addBird(Bird bird){
@@ -35,6 +35,13 @@ public class DatabaseManager {
         ArrayList<Bird> queryResults = this.birdList;
         return queryResults;
     }
+
+    /**
+     * findBird
+     * finds a bird with a certain id
+     * @param id the id of the bird to be found
+     * @return either null (when bird is not found) or the Bird
+     */
     public static Bird findBird(String id)
     {
         for(int i = 0; i < birdList.size(); i++)
@@ -54,44 +61,89 @@ public class DatabaseManager {
      * @param name The name of the bird. If not defined, has length of zero.
      * @param sex The sex of the bird. If male, will be "male", if female, will be "female"
      *            if both will be "both", if undefined will have length of zero
-     * @param birdDate The birth date in string format. Has the format "dd mm yyyy". If not defined
+     * @param birthDate The birth date in string format. Has the format "dd mm yyyy". If not defined
      *                 it will have a length of zero.
      * @param deathDate The death date in string format. Has the format "dd mm yyyy". If not defined
      *                  it will have a length of zero.
      * @return The query results
      */
-    public ArrayList<Bird> searchBirds(String id,String name,String sex,String birdDate,
+    public ArrayList<Bird> searchBirds(String id,String name,String sex,String birthDate,
                                               String deathDate) {
+
         ArrayList<Bird> queryResult = new ArrayList<Bird>();
-        /** TODO
-         *  SOME SEARCH ALGORITHM. MUST BE IMPLEMENTED!!!!!!
-         *  CHANGE THE RETURN VALUE
-         */
-        return this.birdList;
+
+        Bird tempBird;
+        /**This will be replaced with a simple sql statement**/
+        for (int i = 0; i < birdList.size(); i++ )
+        {
+            tempBird = birdList.get(i);
+
+            if(!id.equals("") && !tempBird.id.contains(id))
+                break;
+
+            if(!name.equals("") && !tempBird.name.contains(name))
+                break;
+
+            if(!sex.equals("") && !tempBird.sex.contains(sex))
+                break;
+
+            if(!birthDate.equals("") && !tempBird.birthDate.equals(birthDate))
+                break;
+
+            if(!deathDate.equals("") && !tempBird.deathDate.equals(deathDate))
+                break;
+
+            queryResult.add(tempBird);
+
+        }
+        return queryResult;
     }
 
     /**
      * searchExperiments
      *  Queries the experimentList and returns all birds that match the definitions
-     * @param StudyTitle The title of the experiment. If undefined will have length of zero.
-     * @param StudyType The type of experiment. If undefined will have length of zero.
-     * @param GroupWithinExperiment The group within the experiment. If undefined will have length
+     * @param studyTitle The title of the experiment. If undefined will have length of zero.
+     * @param studyType The type of experiment. If undefined will have length of zero.
+     * @param groupWithinExperiment The group within the experiment. If undefined will have length
      *                              of zero
-     * @param StartDate The start date of the experiment. Has the format "dd mm yyyy". If not defined
+     * @param startDate The start date of the experiment. Has the format "dd mm yyyy". If not defined
      *                 it will have a length of zero.
-     * @param EndDate The end date of the experiment. Has the format "dd mm yyyy". If not defined
+     * @param endDate The end date of the experiment. Has the format "dd mm yyyy". If not defined
      *                 it will have a length of zero.
      * @return The query results
      */
-    public static ArrayList<Experiment> searchExperiments(String StudyTitle,String StudyType,String
-                                                          GroupWithinExperiment,String StartDate,
-                                                          String EndDate) {
+    public static ArrayList<Experiment> searchExperiments(String studyTitle, String studyType, String
+                                                          groupWithinExperiment, String startDate,
+                                                          String endDate) {
         ArrayList<Experiment> queryResult = new ArrayList<>();
-        /** TODO
-         *  SOME SEARCH ALGORITHM. MUST BE IMPLEMENTED!!!!!!
-         *  CHANGE THE RETURN VALUE
-         */
-        return experimentList;
+
+        Experiment tempExperiment;
+
+        /**This will be replaced with a simple sql statement**/
+        for (int i = 0; i < experimentList.size(); i++ )
+        {
+            tempExperiment = experimentList.get(i);
+
+            if(!studyTitle.equals("") && !tempExperiment.getStudyTitle().contains(studyTitle))
+                break;
+
+            if(!studyType.equals("") && !tempExperiment.getStudyType().contains(studyType))
+                break;
+
+            if(!groupWithinExperiment.equals("") && !tempExperiment.getGroupWithinExperiment().contains(groupWithinExperiment))
+                break;
+
+            if(!startDate.equals("") && !tempExperiment.getStartDate().toString().equals(startDate))
+                break;
+
+            if(!endDate.equals("") && !tempExperiment.getEndDate().toString().equals(endDate))
+                break;
+
+            queryResult.add(tempExperiment);
+
+        }
+
+        return queryResult;
     }
 
     public ArrayList<Experiment> getExperiment(){
