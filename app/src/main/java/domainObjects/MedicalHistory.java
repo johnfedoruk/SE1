ef42d59794b9702a0f72df886090401457cd2a02
Kaj <zshadowcast@gmail.com>
2016-03-13 16:55:01 -0500

    MedicalHistory now serializes data for the database, and rebuilds objects

--- app/src/main/java/domainObjects/MedicalHistory.java
+++ app/src/main/java/domainObjects/MedicalHistory.java
@@ -3,6 +3,7 @@ package domainObjects;
 import java.io.Serializable;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 /**
  * Created by Jose Matundan on 2/20/2016.
@@ -18,6 +19,19 @@ public class MedicalHistory implements Serializable {
         this.treatment = "";
         this.Notes ="";
     }
+
+    //Create the class out of the serialized String data.
+    public MedicalHistory(String serialized)
+    {
+        String [] splitArr = serialized.split(",");
+        this.dateOfReport = Calendar.getInstance();
+        this.dateOfReport.setTime(new Date(Long.parseLong(splitArr[0])));
+        this.healthIssue = splitArr[1];
+        this.treatment = splitArr[2];
+        this.Notes = splitArr[3];
+
+    }
+
     public MedicalHistory(Calendar dateOfReport, String healthIssue,String treatment,String Notes)
     {
         this.dateOfReport = dateOfReport;
@@ -40,5 +54,11 @@ public class MedicalHistory implements Serializable {
     public void setHealthIssue(String healthIssue){this.healthIssue = healthIssue;}
     public void setTreatment(String treatment){this.treatment = treatment;}
     public void setNotes(String Notes){this.Notes = Notes;}
+
+    //Serialize the data in MedicalHistory for the Database
+    public String toString()
+    {
+        return String.valueOf(dateOfReport.getTime().getTime()) + "," + healthIssue + "," + treatment + "," +  Notes;
+    }
 
 }
