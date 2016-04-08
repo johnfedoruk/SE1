dca96c58ff8f34fe4852e64df394d5c22130fd4c
Ratstache <landonbutterworth@gmail.com>
2016-04-07 22:35:06 -0400

    adds login screen layout and activity

---
+++ app/src/main/java/presentationLayer/LoginScreen.java
@@ -0,0 +1,14 @@
+package presentationLayer;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+/**
+ * Created by pure__000 on 2016-04-07.
+ */
+public class LoginScreen extends AppCompatActivity {
+
+
+
+}
